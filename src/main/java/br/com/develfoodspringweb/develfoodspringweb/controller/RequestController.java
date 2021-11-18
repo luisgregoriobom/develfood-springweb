74ae4523a53ed26b4bae1ccbcfbe365e040b4ed8
Luis Gregorio <luis.bom@develcode.com.br>
2021-11-18 16:20:47 -0300

    commit with requestcontroller class

---
+++ src/main/java/br/com/develfoodspringweb/develfoodspringweb/controller/RequestController.java
@@ -0,0 +1,23 @@
+package br.com.develfoodspringweb.develfoodspringweb.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Created by Luis Gregorio.
+ *
+ * Controller created to define endpoints and request methods
+ */
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RestController
+@RequestMapping("/api/request")
+public class RequestController {
+
+
+    
+
+}
+
