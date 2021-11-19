fdceab15d3be47b157e9765b181372e4a7b04eee
Luis Gregorio <luis.bom@develcode.com.br>
2021-11-19 18:05:55 -0300

    commit with fix service feat controller

--- src/main/java/br/com/develfoodspringweb/develfoodspringweb/controller/RequestController.java
+++ src/main/java/br/com/develfoodspringweb/develfoodspringweb/controller/RequestController.java
@@ -38,45 +38,9 @@ public class RequestController {
     @GetMapping("/RestaurantRequest/{id}")
     public ResponseEntity<RequestPresent> filterRestaurantId(@PathVariable("id") Long id) {
         RequestDto dto = requestService.searchRequestId(id);
-        RequestPresent present = convertToPresent(dto);
+        RequestPresent present = requestService.convertToPresent(dto);
 
         return ResponseEntity.ok().body(present);
-    }
-
-    /**
-     * Method created to use the attributes coming from the Present classes,
-     * setting and simplifying the information in the request body
-     *
-     * @param dto
-     * @return
-     * @author: Luis Gregorio
-     */
-    private RequestPresent convertToPresent(RequestDto dto) {
-        RequestPresent present = new RequestPresent();
-        present.setId(dto.getId());
-        present.setStatus(dto.getStatus());
-        present.setObs(dto.getObs());
-        present.setDateRequest(dto.getDateRequest());
-        List<PlatePresent> platePresents = new ArrayList<>();
-        dto.getPlate().forEach(plate ->{
-            PlatePresent platePresent = new PlatePresent();
-            platePresent.setObs(plate.getObs());
-            platePresent.setPrice(plate.getPrice());
-            platePresent.setCategory(plate.getCategory());
-            platePresent.setName(plate.getName());
-            platePresents.add(platePresent);
-        });
-
-        UserPresent userPresent = new UserPresent();
-        userPresent.setName(dto.getUser().getName());
-        userPresent.setAddress(dto.getUser().getAddress());
-        userPresent.setCpf(dto.getUser().getCpf());
-        userPresent.setEmail(dto.getUser().getEmail());
-        userPresent.setPhone(dto.getUser().getPhone());
-        present.setUser(userPresent);
-        present.setPlate(platePresents);
-
-        return present;
     }
 
     /**
@@ -88,45 +52,9 @@ public class RequestController {
     @GetMapping("/UserRequest/{id}")
     public ResponseEntity<RequestPresentUser> filterUserId(@PathVariable("id") Long id) {
         RequestDto dto = requestService.searchRequestId(id);
-        RequestPresentUser present = convertToUserPresent(dto);
+        RequestPresentUser present = requestService.convertToUserPresent(dto);
 
         return ResponseEntity.ok().body(present);
-    }
-
-    /**
-     * Method created to use the attributes coming from the Present classes,
-     * setting and simplifying the information in the request body
-     *
-     * @param dto
-     * @return
-     * @author: Luis Gregorio
-     */
-    private RequestPresentUser convertToUserPresent(RequestDto dto) {
-        RequestPresentUser present = new RequestPresentUser();
-        present.setId(dto.getId());
-        present.setStatus(dto.getStatus());
-        present.setObs(dto.getObs());
-        present.setDateRequest(dto.getDateRequest());
-        List<PlatePresentUser> platePresents = new ArrayList<>();
-        dto.getPlate().forEach(plate ->{
-            PlatePresentUser platePresent = new PlatePresentUser();
-            platePresent.setObs(plate.getObs());
-            platePresent.setPrice(plate.getPrice());
-            platePresent.setCategory(plate.getCategory());
-            platePresent.setName(plate.getName());
-            RestaurantPresentUser restaurant = new RestaurantPresentUser();
-            restaurant.setName(plate.getRestaurant().getName());
-            platePresent.setRestaurant(restaurant);
-            platePresents.add(platePresent);
-        });
-
-        UserPresentUser userPresent = new UserPresentUser();
-        userPresent.setAddress(dto.getUser().getAddress());
-        userPresent.setPhone(dto.getUser().getPhone());
-        present.setUser(userPresent);
-        present.setPlate(platePresents);
-
-        return present;
     }
 }
 
--- src/main/java/br/com/develfoodspringweb/develfoodspringweb/service/RequestService.java
+++ src/main/java/br/com/develfoodspringweb/develfoodspringweb/service/RequestService.java
@@ -1,12 +1,22 @@
 package br.com.develfoodspringweb.develfoodspringweb.service;
 
 import br.com.develfoodspringweb.develfoodspringweb.controller.dto.RequestDto;
+import br.com.develfoodspringweb.develfoodspringweb.controller.restaurantCommon.PlatePresent;
+import br.com.develfoodspringweb.develfoodspringweb.controller.restaurantCommon.RequestPresent;
+import br.com.develfoodspringweb.develfoodspringweb.controller.restaurantCommon.UserPresent;
+import br.com.develfoodspringweb.develfoodspringweb.controller.userCommon.PlatePresentUser;
+import br.com.develfoodspringweb.develfoodspringweb.controller.userCommon.RequestPresentUser;
+import br.com.develfoodspringweb.develfoodspringweb.controller.userCommon.RestaurantPresentUser;
+import br.com.develfoodspringweb.develfoodspringweb.controller.userCommon.UserPresentUser;
 import br.com.develfoodspringweb.develfoodspringweb.models.Request;
 import br.com.develfoodspringweb.develfoodspringweb.repository.RequestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created By Luis Gregorio
@@ -30,5 +40,77 @@ public class RequestService {
         RequestDto dto = new RequestDto();
         BeanUtils.copyProperties(request, dto);
         return dto;
+    }
+
+    /**
+     * Method created to use the attributes coming from the Present classes,
+     * setting and simplifying the information in the request body
+     *
+     * @param dto
+     * @return
+     * @author: Luis Gregorio
+     */
+    public RequestPresent convertToPresent(RequestDto dto) {
+        RequestPresent present = new RequestPresent();
+        present.setId(dto.getId());
+        present.setStatus(dto.getStatus());
+        present.setObs(dto.getObs());
+        present.setDateRequest(dto.getDateRequest());
+        List<PlatePresent> platePresents = new ArrayList<>();
+        dto.getPlate().forEach(plate ->{
+            PlatePresent platePresent = new PlatePresent();
+            platePresent.setObs(plate.getObs());
+            platePresent.setPrice(plate.getPrice());
+            platePresent.setCategory(plate.getCategory());
+            platePresent.setName(plate.getName());
+            platePresents.add(platePresent);
+        });
+
+        UserPresent userPresent = new UserPresent();
+        userPresent.setName(dto.getUser().getName());
+        userPresent.setAddress(dto.getUser().getAddress());
+        userPresent.setCpf(dto.getUser().getCpf());
+        userPresent.setEmail(dto.getUser().getEmail());
+        userPresent.setPhone(dto.getUser().getPhone());
+        present.setUser(userPresent);
+        present.setPlate(platePresents);
+
+        return present;
+    }
+
+    /**
+     * Method created to use the attributes coming from the Present classes,
+     * setting and simplifying the information in the request body
+     *
+     * @param dto
+     * @return
+     * @author: Luis Gregorio
+     */
+    public RequestPresentUser convertToUserPresent(RequestDto dto) {
+        RequestPresentUser present = new RequestPresentUser();
+        present.setId(dto.getId());
+        present.setStatus(dto.getStatus());
+        present.setObs(dto.getObs());
+        present.setDateRequest(dto.getDateRequest());
+        List<PlatePresentUser> platePresents = new ArrayList<>();
+        dto.getPlate().forEach(plate ->{
+            PlatePresentUser platePresent = new PlatePresentUser();
+            platePresent.setObs(plate.getObs());
+            platePresent.setPrice(plate.getPrice());
+            platePresent.setCategory(plate.getCategory());
+            platePresent.setName(plate.getName());
+            RestaurantPresentUser restaurant = new RestaurantPresentUser();
+            restaurant.setName(plate.getRestaurant().getName());
+            platePresent.setRestaurant(restaurant);
+            platePresents.add(platePresent);
+        });
+
+        UserPresentUser userPresent = new UserPresentUser();
+        userPresent.setAddress(dto.getUser().getAddress());
+        userPresent.setPhone(dto.getUser().getPhone());
+        present.setUser(userPresent);
+        present.setPlate(platePresents);
+
+        return present;
     }
 }
